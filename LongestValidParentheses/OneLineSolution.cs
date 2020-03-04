a33af43017ebb9bac49cd42cdcd387ca7b7ebbb4
SilentMolusc <jakeeb@ntlworld.com>
2020-03-04 19:03:18 +0000

    OneLineSolution is now truly all on one line

--- LongestValidParentheses/OneLineSolution.cs
+++ LongestValidParentheses/OneLineSolution.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 public class OneLineSolution {
 	static public void Main() {
-		int caseCount = 0;
-		for (int c = 0 ; c < (caseCount = caseCount == 0 ? Int32.Parse(Console.ReadLine()) : caseCount); c++) { Console.WriteLine(Regex.Matches(Console.ReadLine(), "((?'Open'\\()+(?'Close-Open'\\))+)*(?(Open)(?!))", RegexOptions.Compiled).Cast<Match>().Max(m => m.Length)); }
+		Enumerable.Range(0, int.Parse(Console.ReadLine())).ToList().ForEach(c => Console.WriteLine(Regex.Matches(Console.ReadLine(), "((?'Open'\\()+(?'Close-Open'\\))+)*(?(Open)(?!))", RegexOptions.Compiled).Cast<Match>().Max(m => m.Length)));
 	}
 }
